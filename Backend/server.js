6a74fc9fcaa52f6a5f87931f5f9c601d1346b7eb
nirmal3002 <nirmal.m3002@gmail.com>
2025-01-24 06:34:45 +0000

    prod deploy:the vite.config dit

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,12 @@
 import app from "./app.js";
 
 // START SERVER
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
+
+const port = process.env.PORT || 5000;  // Use dynamic port in production
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
