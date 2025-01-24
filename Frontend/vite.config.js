de571acfa27ecf53a102e5145a9fbfc6c8c58713
nirmal3002 <nirmal.m3002@gmail.com>
2025-01-24 06:07:34 +0000

    prod deploy:the vite.config dit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,6 +1,13 @@
-export default {
-  base: '/',
+// export default {
+//   base: '/',
+//   build: {
+//     outDir: 'dist',
+//   },
+// };
+export default defineConfig({
   build: {
     outDir: 'dist',
+    minify: true,
   },
-};
+  // other Vite configurations
+});
